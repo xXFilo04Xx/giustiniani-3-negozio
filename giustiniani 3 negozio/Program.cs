1c6d34dfd9c318776b0a7a6884932d0e599845fe
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-21 08:45:16 +0200

    Add project files.

---
+++ giustiniani 3 negozio/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giustiniani_3_negozio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il prezzo del primo prodotto: ");
+            double prezzo1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci il prezzo del secondo prodotto: ");
+            double prezzo2 = double.Parse(Console.ReadLine());
+            double sconto;
+            double scontato;
+            double totale;
+            if (prezzo1 > prezzo2)
+            {
+                sconto = prezzo2 / 100 * 50;
+                scontato = prezzo2 - sconto;
+                totale = prezzo1 + scontato;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e il prezzo da pagare {totale} euro");
+            }
+            else
+            {
+                sconto = prezzo1 / 100 * 50;
+                scontato = prezzo1 - sconto;
+                totale = prezzo2 + scontato;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e il prezzo da pagare {totale} euro");
+            }
+            Console.ReadLine();
+        }
+    }
+}
